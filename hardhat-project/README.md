370a173273cc5c237d82544d46e9e1a73190c96c
Yash Prakash Balpande <145757143+yashbalpande@users.noreply.github.com>
2025-08-17 00:22:28 +0530

    yash

--- hardhat-project/README.md
+++
@@ -1,7 +0,0 @@
-Hardhat subproject for PolicyManager tests
-
-Usage:
-
-cd hardhat-project
-npm install --legacy-peer-deps
-npx hardhat test
--- hardhat-project/hardhat.config.cjs
+++
@@ -1,8 +0,0 @@
-require('@nomicfoundation/hardhat-toolbox');
-
-module.exports = {
-  solidity: '0.8.19',
-  paths: {
-    sources: 'contracts'
-  }
-};
